d261dda1d7705c97e27a855ebcf96f3cc9d465e0
yahs0105 <yahs0105@gmail.com>
2019-02-17 22:55:25 +0800

    test..

---
+++ day1/test.py
@@ -0,0 +1,5 @@
+# -*- conding:utf-8 -*-
+# @Time    :2019/2/17 下午 10:55
+# @Author  :yahs0105
+# @FileName:test.py
+# @Software:PyCharm
--- week2/day1/test1.py
+++
@@ -1,2 +0,0 @@
-# -*- conding:utf-8 -*-
-# __author__:Jerry Yeh
