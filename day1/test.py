198fefc1c42a29afc70b9675d7cc05036ff7ef69
yahs0105 <yahs0105@gmail.com>
2019-02-17 23:09:49 +0800

    test..

--- day1/test.py
+++
@@ -1,5 +0,0 @@
-# -*- conding:utf-8 -*-
-# @Time    :2019/2/17 下午 10:55
-# @Author  :yahs0105
-# @FileName:test.py
-# @Software:PyCharm
---
+++ day1/test2.py
@@ -0,0 +1,6 @@
+# -*- conding:utf-8 -*-
+# @Time    :2019/2/17 23:09
+# @Author  :yahs0105
+# @FileName:test2.py
+# @Ide     :PyCharm
+
