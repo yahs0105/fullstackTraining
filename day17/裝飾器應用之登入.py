78050760cd355126342463ae78da85b5b0b19e01
yahs0105 <yahs0105@gmail.com>
2019-04-22 20:18:26 +0800

    新增 day17 學習04/22/19

--- day17/裝飾器應用之登入.py
+++ day17/裝飾器應用之登入.py
@@ -4,3 +4,22 @@
 # @FileName:裝飾器應用之登入.py
 # @Ide     :PyCharm
 
+content = input('＞＞＞').strip()
+
+
+num = 0
+for i in content:
+    if i.isalpha():
+        content = content.split(i,' ')
+    num += int(i)
+print(num)
+
+
+
+
+
+
+
+
+
+
