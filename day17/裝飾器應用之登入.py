575341c66be0ce15249814e3a99632616fd49c4a
yahs0105 <yahs0105@gmail.com>
2019-04-28 22:49:20 +0800

    新增 day17 學習04/28/19

--- day17/裝飾器應用之登入.py
+++ day17/裝飾器應用之登入.py
@@ -4,21 +4,43 @@
 # @FileName:裝飾器應用之登入.py
 # @Ide     :PyCharm
 
-content = input('＞＞＞').strip()
 
 
-num = 0
-for i in content:
-    if i.isalpha():
-        content = content.split(i,' ')
-    num += int(i)
-print(num)
+i = 3
+lis = []
+while i > 0:
+    username = input('請輸入用戶名:')
+    passsword = input('請輸入密碼:')
+    with open('info', 'r+', encoding='utf-8')as f:
+        for line in f:
+            lis.append(line)
+    if username == lis[0].strip() and passsword == lis[1].strip():
+        print(f'~~歡迎{username}您的登入~~')
+        break
+    elif username!=lis[0]:
+        choice = input('無此使用者帳戶'
+                       '新註冊請按R:')
+        if choice =='R' or choice =='r':
+            uname = input('請輸入新用戶名:')
+            pword = input('請輸入新密碼:')
+            lis = [uname,pword]
+            with open('info', 'r+', encoding='utf-8')as f:
+                f.read()
+                f.write(f'{lis}\n')
+        print('恭喜註冊成功,請重新登入')
+    else:print(f'查無帳號,己輸入{i-1}次')
+
+    i-=1
 
 
-
-
-
-
+# with open('小護士','r+',encoding='utf-8') as f,open('小護士.bak','w+',encoding='utf-8') as f2:
+#     for line in f:
+#         if '中國' in line:
+#             line = line.replace('中國','台灣')
+#         f2.write(line)
+# import os
+# os.remove('小護士')
+# os.rename('小護士.bak','小護士')
 
 
 
