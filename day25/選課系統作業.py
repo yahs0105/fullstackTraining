695e4e015e08b8a711588b403dbc4588bd290213
yahs0105 <yahs0105@gmail.com>
2019-07-28 14:06:38 +0800

    update 0728

---
+++ day25/選課系統作業.py
@@ -0,0 +1,14 @@
+class School:
+    def __init__(self, name, addr):
+        self.name = name
+        self.addr = addr
+
+
+class courst:
+    def __init__(self,name,price,period,school):
+        self.name=name
+        self.price=price
+        self.period=period
+        self.school=school
+
+s1=School()
